93407beee23a4ca4c5d41a9eb72b451506ec0f3f
kaiyaeeo <kaiyaeeo@gmail.com>
2025-10-20 20:51:38 +0700

    penambahan dokumentasi

--- tipe-data.cpp
+++ tipe-data.cpp
@@ -4,12 +4,46 @@ using namespace std;
 int main ()
 {
     //Tipe Data
-    int Desimal = 5;            // Integer
-    float Angka = 5.99;         // Floating point number
-    double Angka2 = 9.98;       // Floating point number
-    char Huruf = 'D';           // Character
+    int Desimal = 7;            // Integer
+    float Angka = 7.01;         // Floating point number
+    double Angka2 = 7.02;       // Floating point number
+    char Huruf = 'B';           // Character
     bool benarsalah = true;     // Boolean
     string teks = "Ini String"; // String
+
+
+    //Variabel
+    //Variabel adalah tempat penyimpanan data yang memiliki nilai beragam
+    int Desimal = 7;
+    //Desimal menjadi variabel yang menyimpan nilai integer 7
+    float Angka = 7.01;
+    //Angka menjadi variabel uang menyimpan nilai 7.01
+    double Angka2 = 7.02;
+    //Angka2 menjadi variabel yang menyimpan nilai 7.02
+    //Double dapat menyimpan nilai 2 kali lipat lebih banyak daripada float
+    char Huruf = 'B';
+    //Huruf menjadi variabel yang menyimpan data B
+    //char ditandai dengan tanda kutip tunggal dan hanya bisa menyimpan satu karakter saja
+    bool benarsalah = true;
+    //benarsalah menyimpan nilai bool true
+    string teks = "Ini String";
+    //variabel teks menyimpan data Ini String
+    //String ditandai dengan tanda kutip ganda dan bisa menyimpan banyak karakter sekaligus
+    const int jam = 24; //Constants
+    //Const digunakan untuk menyimpan data yang tidak bisa diubah-ubah
+
+
+    int i = 1, j = 2, r = 3;
+    /* Kita bisa menyebutkan berbagai variabel sekaligus dengan syarat variabel-variabel
+    tersebut memiliki tipe data yang sama.*/
+    
+    /* Deklarasi dan Inisialisasi Variabel
+    Deklarasi Variabel digunakan agar kompiler mengetahui tipe data dari variabel.
+    Inisialisasi adalah adalah tindakan memberikan nilai awal ke variabel yang telah Anda deklarasikan. 
+    Inisialisasi adalah opsional, tetapi seringkali Anda akan memberikan 
+    nilai awal ke variabel untuk menghindari nilai acak atau sampah.
+    Sebagai contoh, i pada kode di atas diinisialisasi dengan = 1
+    */
 
 
 
