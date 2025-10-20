8897e5936a9d34aa0dc4ba1565bb07502b2f3c42
kaiyaeeo <kaiyaeeo@gmail.com>
2025-10-20 18:57:41 +0700

    penambahan dokumentasi belajar cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Komentar dalam C++ dapat menggunakan garis miring dan bintang ataupun dua garis miring
+Jika memiliki komentar banyak maka gunakan garis miring dan bintang. Namun jika sedikit
+bisa menggunakan dua garis miring
+*/
+
+#include <iostream> //Header file library
+using namespace std;
+/*Digunakan untuk menggunakan nama untuk objek dan variabel yang ada di library
+Jika tidak menggunakan 'using namespace std;' maka harus menuliskan std:: setiap akan
+menggunakan nama untuk objek dan variabel yang ada di library
+Dua komponen di atas selalu digunakan setiap membuat program C++ */
+
+int main() //Function
+{ //Semua kode yang berada di dalam kurung akan dieksekusi oleh int main function
+    cout<< "Hello World"<<"\n"; //"\n" digunakan untuk memisahkan baris
+    cout<< 9 ; //cout digunakan untuk mencetak string atau angka yang dituliskan programmer
+return 0; //Digunakan untuk mengakhiri int main function
+} 
---
+++ tipe-data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    //Tipe Data
+    int Desimal = 5;            // Integer
+    float Angka = 5.99;         // Floating point number
+    double Angka2 = 9.98;       // Floating point number
+    char Huruf = 'D';           // Character
+    bool benarsalah = true;     // Boolean
+    string teks = "Ini String"; // String
+
+
+
+
+
+    return 0;
+}
