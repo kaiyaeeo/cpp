59906f64706c8cce0faf0f44018928621ac4e478
kaiya <kaiyaeeo@gmail.com>
2025-10-21 22:37:57 +0700

    menambah kondisional
    Fixed syntax errors and added switch statement to handle days of the week.

--- conditional.cpp
+++ conditional.cpp
@@ -39,6 +39,31 @@ int main()
         cout<< "Anda bukan shift D";
     }
 
-
+    int hari = 7
+    swtich (hari) { //switch digunakan untuk mengeksekusi banyak kondisi sekaligus
+        //Switch akan dieksekusi sekali, jika terdapat kecocokan dalam salah satu case, maka blok case tersebut akan dieksekusi
+        case 1:
+            cout<<"Senin";
+            break;
+        case 2:
+            cout<<"Selasa";
+            break;
+        case 3:
+            cout<<"Rabu";
+            break;
+        case 4:
+            cout<<"Kamis";
+            break;
+        case 5:
+            cout<<"Jumat";
+            break;
+        case 6:
+            cout<<"Sabtu";
+            break;
+        case 7:
+            cout<<"Minggu";
+            break;
+        
     return 0;
+
 }
